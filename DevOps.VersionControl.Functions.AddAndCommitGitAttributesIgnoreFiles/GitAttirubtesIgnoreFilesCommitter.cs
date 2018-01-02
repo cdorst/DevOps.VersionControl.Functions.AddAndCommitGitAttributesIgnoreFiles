9f9bb86ab0c663099291049d1db9f74ca57b271e
Christopher Dorst <chris@dorst.email>
2018-01-02 01:12:46 -0600

    Add GitAttirubtesIgnoreFilesCommitter

---
+++ DevOps.VersionControl.Functions.AddAndCommitGitAttributesIgnoreFiles/GitAttirubtesIgnoreFilesCommitter.cs
@@ -0,0 +1,17 @@
+﻿using DevOps.VersionControl.Structures.GitCommitUser;
+using static DevOps.VersionControl.Functions.AddAndCommitGitChanges.GitChangeAdderAndCommitter;
+using static Metaproject.Cdn.Containers.VersionControl.Helpers.VersionControlContentHelper;
+
+namespace DevOps.VersionControl.Functions.AddAndCommitGitAttributesIgnoreFiles
+{
+    public static class GitAttirubtesIgnoreFilesCommitter
+    {
+        private const string CommitMessage = "Add .gitattributes and .gitignore files";
+
+        public static void AddAndCommitGitAttributeIgnoreFiles(string repositoryDirectory, UserInfo? user = null)
+        {
+            SaveGitAttributeIgnoreFiles(repositoryDirectory);
+            AddAndCommitChanges(repositoryDirectory, CommitMessage, user);
+        }
+    }
+}
